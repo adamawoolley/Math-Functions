4350baaa2d09c2fed54e9518da3cbb7e9c6f73d2
adamawoolley <adamawoolley@gmail.com>
2017-08-29 18:53:53 -0700

    Create factor.py
    Find the factors of any number!

---
+++ factor.py
@@ -0,0 +1,17 @@
+print('To find a factor just type factor(number you want to find the factors of) into the console and type enter')
+
+def factor(test_number):
+    
+    divisor = 1
+    factors = str('')
+
+    while test_number >= divisor:
+
+        if test_number % divisor == 0:
+            factors += str(divisor)
+            factors += str(' ')
+            divisor += 1
+        else:
+            divisor += 1
+
+    print('The factors of ' + str(test_number) + ' are ' + str(factors))
